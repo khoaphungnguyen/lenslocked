5ff115ff43d5b42a0132fdb5a215d3fe44cb153d
Rex Phung <khoatuanphungnguyen@gmail.com>
2022-01-18 17:31:50 -0800

    Using Chi for routing http requests

--- go.mod
+++ go.mod
@@ -2,23 +2,4 @@ module github.com/khoaphungnguyen/lenslocked
 
 go 1.17
 
-require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
-	github.com/bmatcuk/doublestar v1.3.4 // indirect
-	github.com/cortesi/modd v0.0.0-20211215124449-6083f9d1c171 // indirect
-	github.com/cortesi/moddwatch v0.0.0-20210222043437-a6aaad86a36e // indirect
-	github.com/cortesi/termlog v0.0.0-20210222042314-a1eec763abec // indirect
-	github.com/fatih/color v1.13.0 // indirect
-	github.com/go-chi/chi/v5 v5.0.7 // indirect
-	github.com/mattn/go-colorable v0.1.9 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/rjeczalik/notify v0.0.0-20181126183243-629144ba06a1 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/net v0.0.0-20210323141857-08027d57d8cf // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-	golang.org/x/term v0.0.0-20210317153231-de623e64d2a6 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
-	mvdan.cc/sh/v3 v3.3.0-0.dev.0.20210224101809-fb5052e7a010 // indirect
-)
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,21 +18,36 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:rex@test.com\">rex@test.com</a>.</p>")
 }
 
-type Router struct{}
+func fagHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<h1>FAG page</h1>
+	<p>
+	<strong>Q: Is there a free version?</strong><br>
+	Yes: We offer a free trial for 30 days on any paid plans. <br>
+	<br>
+	<strong>Q: What are your support hours?</strong><br>
+	We have support staff answering email 24/7, though response time may be a bit slower on weekends. <br>
+	<br>
+	<strong>Q: How do I contact support?</strong><br>
+	Email us -  <a href=\"mailto:rex@test.com\">rex@test.com</a>.</p> <br>
+	</p>`)
+}
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", 401)
-	}
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, r.URL.Path)
+
 }
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/articles/{date}", getArticle)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/fag", fagHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on: 3000....")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
