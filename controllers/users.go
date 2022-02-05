8528e9fcd40d3fb8ca64e4406f63d43a94899996
Rex Phung <khoatuanphungnguyen@gmail.com>
2022-02-05 10:53:50 -0800

    Decouple with interfaces

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/khoaphungnguyen/lenslocked/views"
 )
 
 type Users struct {
 	Template struct {
-		New views.Template
+		New Template
 	}
 }
 
